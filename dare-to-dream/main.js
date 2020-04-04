bd18e36f8394647b16ec00089f8355edcf803de4
María del Mar <mdmperez91@gmail.com>
2020-04-04 12:48:26 +0200

    Add other projects

---
+++ dare-to-dream/main.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const btnShow = document.querySelector('.section-stars__content-follow--text');
+
+function showMore() {
+    const titlePpal = document.querySelector('.section-stars__content-title--title');
+    titlePpal.classList.add('hide');
+    btnShow.classList.add('hide');
+
+    const section2 = document.querySelector('.section-to-do');
+    section2.classList.remove('hide');
+}
+
+btnShow.addEventListener('click', showMore);
+
+const input = document.getElementById('dreams');
+const ulList = document.querySelector('.section-to-do__list');
+
+function showDreams() {
+    if(input.value === '') {
+        const contentMsg = document.querySelector('.section-to-do__content-msg');
+        contentMsg.classList.remove('section-to-do__content-msg');
+        contentMsg.classList.add('show-msg');
+
+        const removeMsg = () => {
+            contentMsg.classList.remove('show-msg');
+            contentMsg.classList.add('section-to-do__content-msg');
+          };
+          setTimeout(removeMsg, 6000);
+
+        function closeTooltip() {
+        const close = event.currentTarget;
+        close.parentElement.classList.remove('show-msg'); 
+        close.parentElement.classList.add('section-to-do__content-msg'); 
+        }
+
+        const closeElement = document.querySelector('.section-to-do__content-msg--close');
+        closeElement.addEventListener('click', closeTooltip);
+        
+    } else {
+        let li = document.createElement('LI');    
+        li.innerHTML = input.value ;
+    
+        const inputCheck = document.createElement("INPUT");
+        inputCheck.setAttribute("type", "checkbox");
+            
+        let closeElement = document.createElement('A');
+        closeElement.setAttribute('href', '#');
+        closeElement.innerHTML= 'X';
+    
+        ulList.appendChild(li); 
+        li.appendChild(inputCheck);
+        li.appendChild(closeElement);
+    
+        input.value = '';
+    
+        function dreamChecked() {
+            if(inputCheck.checked === true) {
+                inputCheck.parentElement.classList.toggle('completed');
+            } else if (inputCheck.checked === false) {
+                li.classList.remove('completed');
+            }
+        }
+    
+        inputCheck.addEventListener('change' , dreamChecked);
+        
+        function clearDream() {
+            closeElement.parentElement.remove();
+        }
+        closeElement.addEventListener('click', clearDream);
+    }
+    
+}
+ 
+const btn = document.querySelector('.section-to-do__btn');
+btn.addEventListener('click', showDreams);
+
+
+
+
